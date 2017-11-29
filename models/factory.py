79ad1b22ecef97c89ebf5a0ca8a19e1319c535de
Miguel Rodriguez <m.rodriguezs1990@gmail.com>
2017-11-29 21:29:41 -0200

    update configs

--- models/factory.py
+++ models/factory.py
@@ -13,6 +13,7 @@ def model_factory(model_name, img_rows, img_cols, channel, num_classes, dropout_
         model = densenet121_model(img_rows, img_cols, channel, num_classes=num_classes, dropout_rate=dropout_keep_prob)
 
     if checkpoint != '':
+        print("Loading checkpoint:", checkpoint)
         model.load_weights(checkpoint, by_name=True)
 
     return model
