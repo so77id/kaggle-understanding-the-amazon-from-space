fe106fc4a9e9184443abad0992e5c7009f4262c9
Miguel Rodriguez <m.rodriguezs1990@gmail.com>
2017-11-30 13:01:49 -0200

    add new predicters

--- scripts/predicter.sh
+++ scripts/predicter.sh
@@ -2,17 +2,11 @@ GPU=false
 CUDA_VISIBLE_DEVICES=0
 
 # DENSENET
-make run-predict GPU=$GPU CONFIG_FILE=./configs/experimental-configs/densenet_121-24-11-7-config.json CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES
-
-make run-predict GPU=$GPU CONFIG_FILE=./configs/experimental-configs/densenet_121-25-11-1-config.json CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES
+make run-predict GPU=$GPU CONFIG_FILE=./configs/experimental-configs/densenet_121-30-11-7-config.json CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES
 
 # RESNET
-make run-predict GPU=$GPU CONFIG_FILE=./configs/experimental-configs/resnet_50-24-11-4-config.json CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES
+make run-predict GPU=$GPU CONFIG_FILE=./configs/experimental-configs/resnet_50-30-11-5-config.json CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES
 
 # INCEPTION
-make run-predict GPU=$GPU CONFIG_FILE=./configs/experimental-configs/inception-v4-23-11-7-config.json CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES
-
-make run-predict GPU=$GPU CONFIG_FILE=./configs/experimental-configs/inception-v4-23-11-9-config.json CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES
-
-make run-predict GPU=$GPU CONFIG_FILE=./configs/experimental-configs/inception-v4-24-11-1-config.json CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES
+make run-predict GPU=$GPU CONFIG_FILE=./configs/experimental-configs/inception-v4-30-11-3-config.json CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES
 
